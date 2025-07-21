d3196d8b656036d2490bd0ed987f30e575a9f35a
Rose <131393461+Kendi044@users.noreply.github.com>
2025-07-21 21:43:25 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,6 @@
 # arithmetic_operations.py
 
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     """
     Performs basic arithmetic operations.
 
@@ -23,6 +23,6 @@ def perform_operation(num1: float, num2: float, operation: str):
             return "Error: Cannot divide by zero."
         else:
             return num1 / num2
-    else:
+     else:
         return "Error: Invalid operation."
        
